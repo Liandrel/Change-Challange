a9720941a3bd8ae28ac307069a1a8df762395f8a
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-03 21:51:40 +0200

    Calculate Change method
    created method to calculate and put into list of string change by amount and denomination name

--- ChangeChall/Program.cs
+++ ChangeChall/Program.cs
@@ -10,6 +10,39 @@ denominations.Add(new DenominationInfo { Name = "Quarter", Amount = 0.25M });
 denominations.Add(new DenominationInfo { Name = "Dime", Amount = 0.10M });
 denominations.Add(new DenominationInfo { Name = "Penny", Amount = 0.01M });
 
+static List<string> CalculateChange(decimal amountOwed, decimal amountPaid, List<DenominationInfo> denominations)
+{
+    List<string> output = new();
+    decimal change = amountPaid - amountOwed;
+
+    if(change < 0)
+    {
+        output.Add("You need to pay more.");
+    }
+    else if( change == 0)
+    {
+        output.Add("No change required.");
+
+    }
+    else
+    {
+        var orderedDenominations = denominations.OrderByDescending(x => x.Amount);
+        int count = 0;
+
+        foreach ( var denomination in orderedDenominations)
+        {
+            (change, count) = CalculateSpecificChange(change, denomination.Amount);
+        
+            if(count > 0)
+            {
+                output.Add($"{count} {denomination.Name}");
+            }
+        }
+    }
+
+    return output;
+}
+
 static (decimal remainder, int numberOfItems) CalculateSpecificChange(decimal amount, decimal denomination)
 {
     (decimal remainder, int numberOfItems) output = (0, 0);
