12691cd40fd44d607c7a4d5f6c035cffebd6acc4
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-03 21:50:29 +0200

    Calculate specific change method
    created method to calculate change by amout and denomination put as argument

--- ChangeChall/Program.cs
+++ ChangeChall/Program.cs
@@ -9,3 +9,13 @@ denominations.Add(new DenominationInfo { Name = "Nickel", Amount = 0.05M });
 denominations.Add(new DenominationInfo { Name = "Quarter", Amount = 0.25M });
 denominations.Add(new DenominationInfo { Name = "Dime", Amount = 0.10M });
 denominations.Add(new DenominationInfo { Name = "Penny", Amount = 0.01M });
+
+static (decimal remainder, int numberOfItems) CalculateSpecificChange(decimal amount, decimal denomination)
+{
+    (decimal remainder, int numberOfItems) output = (0, 0);
+
+    output.numberOfItems = (int)Math.Floor(amount / denomination);
+    output.remainder = amount - (output.numberOfItems * denomination);
+
+    return output;
+}
