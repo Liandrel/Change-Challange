694e6f9fb94ecc65e760b170a171f774a91728c6
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-03 21:49:23 +0200

    DenominationInfo Class
    Added Denomination class and filled list with example data in UI

---
+++ ChangeChall/DenominationInfo.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeChall
+{
+    public class DenominationInfo
+    {
+        public decimal Amount { get; set; }
+        public string Name { get; set; }
+    }
+}
--- ChangeChall/Program.cs
+++ ChangeChall/Program.cs
@@ -1,1 +1,11 @@
-﻿
+using ChangeChall;
+
+
+List<DenominationInfo> denominations = new();
+denominations.Add(new DenominationInfo { Name = "One Dollar Bill", Amount = 1M });
+denominations.Add(new DenominationInfo { Name = "Five Dollar Bill", Amount = 5M });
+denominations.Add(new DenominationInfo { Name = "Ten Dollar Bill", Amount = 10M });
+denominations.Add(new DenominationInfo { Name = "Nickel", Amount = 0.05M });
+denominations.Add(new DenominationInfo { Name = "Quarter", Amount = 0.25M });
+denominations.Add(new DenominationInfo { Name = "Dime", Amount = 0.10M });
+denominations.Add(new DenominationInfo { Name = "Penny", Amount = 0.01M });
